b6d55b0018c9fb0b6adb21ae9e0f5669f277f49b
pbon98 <pbaughan15@gmail.com>
2023-03-30 21:22:48 -0400

    intermediate-algorithm-scripting

---
+++ step2.js
@@ -0,0 +1,21 @@
+function diffArray(arr1, arr2) {
+    const newArr = [];
+  //Searches through first array
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr2.indexOf(arr1[i]) === -1) {
+        newArr.push(arr1[i])
+      }
+    }
+    //Searches through second array
+    for (let i = 0; i < arr2.length; i++) {
+      if (arr1.indexOf(arr2[i]) === -1) {
+        newArr.push(arr2[i])
+      }
+    }
+    
+    
+    return newArr;
+  
+  }
+  
+  diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
