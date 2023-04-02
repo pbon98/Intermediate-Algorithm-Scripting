4c2ea89d8061a57329a70e7d8e14088aa053fd4e
pbon98 <pbaughan15@gmail.com>
2023-04-02 02:40:39 -0400

    intermediate-algorithm-scripting

---
+++ step12.js
@@ -0,0 +1,15 @@
+function sumFibs(num) {
+    let previousNumber = 0
+    let currentNumber = 1
+    let result = 0
+    while (currentNumber <= num) {
+      if (currentNumber % 2 !== 0) {
+        result += currentNumber
+      }
+      currentNumber += previousNumber
+      previousNumber = currentNumber - previousNumber
+    }
+    return result
+  }
+  
+  console.log(sumFibs(4));
