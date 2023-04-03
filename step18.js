dfd6cf32ebe2487057f9ab15da12a974daeb3f2a
pbon98 <pbaughan15@gmail.com>
2023-04-02 21:12:10 -0400

    intermediate-algorthm-scripting

---
+++ step18.js
@@ -0,0 +1,6 @@
+function truthCheck(collection, pre) {
+
+    return collection.every(object => object [pre]);
+  }
+  
+  console.log(truthCheck([{name: "Quincy", role: "Founder", isBot: false}, {name: "Naomi", role: "", isBot: false}, {name: "Camperbot", role: "Bot", isBot: true}], "isBot"));
